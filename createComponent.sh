254d89b0e6bcf6ff17a2e140fd7fdf173450691f
Michael Curran <65498724+MiCurran@users.noreply.github.com>
2021-09-06 13:27:59 -0600

    Update createComponent.sh
    adding bash script

--- createComponent.sh
+++ createComponent.sh
@@ -1,1 +1,31 @@
+#!/bin/sh
+while getopts f:a: flag
+do
+    case "${flag}" in
+        f) filename=${OPTARG};;
+        a) useFile=${OPTARG};;
+    esac
+done
 
+mkdir ./views/$filename;
+cd ./views/$filename;
+touch $filename.js;
+touch $filename.scss;
+if [ $useFile ]
+then
+echo 'creating component with use file'
+touch use$filename.js
+fi
+
+jsfile=$filename.js
+echo "import { useState } from 'react';" > $jsfile
+echo "import classes from './$filename.module.scss';" >> $jsfile
+echo ' ' >> $jsfile
+echo "export const $filename = (props) => {" >> $jsfile
+echo "    return (" >> $jsfile
+echo "        <p>I'm the new $filename component look at meeeeee</p>" >> $jsfile
+echo "    );" >> $jsfile
+echo '};' >> $jsfile
+echo >> $jsfile
+
+echo "Created a component named: $filename";
